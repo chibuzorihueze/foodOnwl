0fe8abab937bda5b7d4202be154efdd7a8c6a24e
unknown <krisykord@gmail.com>
2023-03-01 04:56:55 -0800

    setting working environment

---
+++ src/Components/nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./foodOnwl.png";
+
+const Nav = () => {
+  return (
+    <div>
+      <img src={logo} alt="imagelogo" className="w-48 h-48" />
+    </div>
+  );
+};
+
+export default Nav;
