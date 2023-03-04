1faf1311ffd2cd5d3636f15331de8982cec48498
unknown <krisykord@gmail.com>
2023-03-03 22:12:21 -0800

    navbar component setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from './Components/nav'
 
 
+
 const App = () => {
   return (
     <Nav/>
--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import logo from "./foodOnwl.png";
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
 
-const Nav = () => {
+
+
+const Navbar = () => {
   return (
-    <div>
-      <img src={logo} alt="imagelogo" className="w-48 h-48" />
-    </div>
+    <nav className="bg-white shadow-m">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex-shrink-0 flex items-center">
+            <img className="h-40 w-48" src={logo} alt="Logo" />
+          </div>
+          <div className="flex items-center">
+            <div className="mr-8"><AiOutlineShoppingCart/></div>
+            <button className="bg-[#229900] hover:bg-[#FF9900] text-white font-bold py-2 px-4 rounded">
+              Sign up
+            </button>
+            <div className="rounded-full border border-[#BDBDBD] bg-[#EAEAEA] ml-6 p-2.5 "><AiOutlineUser/></div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
-export default Nav;
+export default Navbar;
