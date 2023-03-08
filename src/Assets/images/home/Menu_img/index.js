42cd901d2b5a839afe54a6d2aca61279f9b47a32
unknown <krisykord@gmail.com>
2023-03-08 03:46:24 -0800

    setting up hero for menu section

---
+++ src/Assets/images/home/Menu_img/index.js
@@ -0,0 +1,2 @@
+export { default as ChikenRep} from "./ChickenRep.png";
+export { default as Menuhero} from "./Menuhero.png";
--- src/pages/website/Home/Menu.js
+++ src/pages/website/Home/Menu.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import Navbar from './../../../components/nav2';
+import React from "react";
+import { ChikenRep, Menuhero } from "../../../assets/images/home/Menu_img";
+import Navbar from "./../../../components/nav2";
 
 const Menu = () => {
   return (
     <>
-    <Navbar/>
-    </>
-  )
-}
+      <Navbar />
+      <div className="flex justify-center items-center w-[75%]">
+        <div className="mx-auto my-auto relative">
+          <img className="w-[810px] h-[200px]" src={Menuhero} alt="hero img" />
+          <img style={{ position: 'absolute', top: '90px', left: '50px' }} src={ChikenRep} alt="food-logo"/>
+        </div>
+      </div>
 
-export default Menu
+      <div>
+        <h1>Chiken Republic</h1>
+        <p>Chiken Republic</p>
+        
+      </div>
+    </>
+  );
+};
+
+export default Menu;
