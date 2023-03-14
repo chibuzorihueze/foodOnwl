3863c809bd1f2024b2d72b40043a1944adfaa328
unknown <krisykord@gmail.com>
2023-03-14 04:12:30 -0700

    navbar fixes

--- src/Components/blocks/navigations/TopNav.js
+++ src/Components/blocks/navigations/TopNav.js
@@ -6,24 +6,24 @@ import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
 
 const TopNav = ({ hasSearch }) => {
   return (
-    <Navbar expand="md" className=" pt-0 shadow-m mr-8">
+    <Navbar expand="md" className=" pt-0 shadow-m px-2">
       <Navbar.Brand>
-        <div className="relative w-28 h-20 sm:w-40 sm:h-28">
-          <img src={logo} alt="Logo" className="border absolute h-full w-full" />
+        <div className="relative w-28 h-20 ">
+          <img src={logo} alt="Logo" className="absolute h-full w-full" />
         </div>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         {hasSearch && (
-          <Form inline className="flex-1 justify-center">
+          <Form inline className="flex-1">
             <FormControl
               type="text"
               placeholder="Search menu..."
-              className="mr-sm-2 w-96 mx-auto"
+              className="mr-sm-2 w-72 mx-auto"
             />
           </Form>
         )}
-        <Nav className="ml-auto align-items-center">
+        <Nav className="align-items-center">
           <Nav.Link href="#" className="mr-4">
             <AiOutlineShoppingCart size={20} />
           </Nav.Link>
